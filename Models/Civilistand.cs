96c60ca475cfcf338ceb7f5a6ec67a73d8286fc8
Rikke Barsten <ribar@vejle.dk>
2020-01-14 13:56:34 +0100

    First commit

---
+++ Models/Civilistand.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ydelsesberegner.Models
+{
+    public class Valgmuligheder
+    {
+
+   
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Ydelsesberegner.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public string Navn {get; set;}
+
+        [BindProperty]
+         public string Civilstand { get; set; }
+
+         public string[] Civilstande = new[] { "Gift", "Ugift" };
+
+
+           
+
+    public void OnGet()
+        {
+            
+        }
+
+    public void OnPost()
+        {
+            if(Civilstand == "Gift"){
+                Response.Redirect("Error");
+            }
+        }
+    }
+}
