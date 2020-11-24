94e0d5e13d5c92b8e533629d676cc3eb3a9dc7f4
Rikke Barsten <ribar@vejle.dk>
2020-11-24 13:21:39 +0100

    Fjerne

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace Ydelsesberegner
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-             app.UseCookiePolicy();
+            // app.UseCookiePolicy();
 
             app.UseRouting();
 
