1e09f95ef4bb4a71c5d74e3dfe9681d22cd7b17c
iamgregorj <iam@gregorj.org>
2022-12-30 08:25:40 +0100

    Opdateret med 2023 satser

--- Pages/Resultat.cshtml.cs
+++ Pages/Resultat.cshtml.cs
@@ -37,31 +37,34 @@ namespace Ydelsesberegner.Pages
         }
 
 
-        public const string Text1Del1 = "<p>På baggrund af de oplysninger, du har indtastet, ser det ud til, at du kan få ";
+        public const string Text1Del1 = "<p>På baggrund af de oplysninger, du har indtastet, ser det ud til, at du kan få <b>";
         
         
-        public const string KHText1Del2 = " kroner i kontanthjælp om måneden før skat.</p>" +
+        public const string KHText1Del2 = "</b> kroner i kontanthjælp om måneden før skat.</p>" +
                     "<p>Når du modtager kontanthjælp skal du stå til rådighed for aktiviteter op til 37 timer om ugen." +
-                    "Det svarer til en timeløn på ";
+                    "Det svarer til en timeløn på <b>";
 
-        public const string UYText1Del2 = " kroner i uddannelseshjælp om måneden før skat.</p>" +
+        public const string UYText1Del2 = "</b> kroner i uddannelseshjælp om måneden før skat.</p>" +
                     "<p>Når du modtager uddannelseshjælp skal du stå til rådighed for aktiviteter op til 37 timer om ugen." +
-                    "Det svarer til en timeløn på ";
+                    "Det svarer til en timeløn på <b>";
 
-         public const string SHText1Del2 = " kroner i <abbr title='selvforsørgelses- og hjemrejseydelse'>S&H-ydelse</abbr> om måneden før skat.</p>" +
+         public const string SHText1Del2 = "</b> kroner i <abbr title='selvforsørgelses- og hjemrejseydelse'>S&H-ydelse</abbr> om måneden før skat.</p>" +
                     "<p>Når du modtager <abbr title='selvforsørgelses- og hjemrejseydelse'>S&H-ydelse</abbr> skal stå til rådighed for aktiviteter op til 37 timer om ugen. <br/>" +
-                    "Det svarer til en timeløn på ";
+                    "Det svarer til en timeløn på <b>";
 
-        public const string Text1Del3 = " kroner før skat. Det betyder, at du eksempelvis kan blive bedt om at deltage i nyttejob, hvor du skal hjælpe med at vedligeholde kommunens grønne arealer." +
+        public const string Text1Del3 = "</b> kroner før skat. Det betyder, at du eksempelvis kan blive bedt om at deltage i nyttejob, hvor du skal hjælpe med at vedligeholde kommunens grønne arealer." +
                     "</p>" +
-
-                    "<h3>Det kan betale sig at arbejde</h3>" +
+                    "<div class=\"flex-important\">" +
+                    "<div class=\"important\">" +
+                    "<h3>Det kan betale sig at arbejde!</h3>" +
                     "<p>Vælger du i stedet at tage et job, kan du tjene flere penge i timen.</p>" +
                     "<ul>" +
-                    "<li>Et avisbud får ca. 100 kroner i timen.</li>" +
+                    "<li>Et avisbud får ca. 115 kroner i timen.</li>" +
                     "<li>En kassemedarbejder får ca. 130 kroner i timen.</li>" +
                     "<li>En faglært får ca. 200 kroner i timen.</li>" +
-                    "</ul>";
+                    "</ul>" +
+                    "</div>" +
+                    "</div>";
 
         public const string KHText1Del4 ="<h3>Så få timer om ugen skal du arbejde for at tjene det, der svarer til kontanthjælp. </h3>" +
                     "<p>Hvis du tager et job i stedet for kontanthjælp kan du nøjes med at arbejde færre timer, end du skal stå til rådighed.</p>";
@@ -106,20 +109,20 @@ namespace Ydelsesberegner.Pages
             Hashtable muligeUdfald = new Hashtable();
         
 
-            muligeUdfald.Add("A", new Udfald("15.355", "96", "kh", 35, 27, 18));
-            muligeUdfald.Add("B", new Udfald("11.554", "72", "kh", 27, 21, 13));
-            muligeUdfald.Add("C", new Udfald("14.677", "92", "kh", 34, 26, 17));
-            muligeUdfald.Add("D", new Udfald("10.268", "64", "kh", 24, 18, 12));
-            muligeUdfald.Add("E", new Udfald("7.448", "46", "kh", 17, 13, 9));
-            muligeUdfald.Add("F", new Udfald("3.594", "22", "kh", 8, 6, 4));
-            muligeUdfald.Add("G", new Udfald("6.331", "39", "uy", 15, 11, 7));
-            muligeUdfald.Add("H", new Udfald("2.728", "17",  "uy", 6, 5, 3));
-            muligeUdfald.Add("I", new Udfald("12.663", "79", "uy", 29, 22, 15));
-            muligeUdfald.Add("K", new Udfald("8.862", "55", "uy", 20, 16, 10));
-            muligeUdfald.Add("L", new Udfald("12.283", "77", "sh", 28, 22, 14));
-            muligeUdfald.Add("M", new Udfald("8.596", "54", "sh", 20, 15, 10));
-            muligeUdfald.Add("N", new Udfald("6.142", "38", "sh", 14, 11, 7));
-            muligeUdfald.Add("O", new Udfald("2.646", "17", "sh", 6, 5, 3));
+            muligeUdfald.Add("A", new Udfald("15.874", "96", "kh", 32, 28, 18));
+            muligeUdfald.Add("B", new Udfald("11.944", "72", "kh", 24, 21, 14));
+            muligeUdfald.Add("C", new Udfald("15.172", "92", "kh", 30, 27, 18));
+            muligeUdfald.Add("D", new Udfald("10.615", "64", "kh", 21, 19, 12));
+            muligeUdfald.Add("E", new Udfald("7.699", "46", "kh", 15, 14, 9));
+            muligeUdfald.Add("F", new Udfald("3.715", "22", "kh", 7, 7, 4));
+            muligeUdfald.Add("G", new Udfald("6.545", "39", "uy", 13, 12, 8));
+            muligeUdfald.Add("H", new Udfald("2.820", "17",  "uy", 6, 5, 3));
+            muligeUdfald.Add("I", new Udfald("13.090", "79", "uy", 26, 23, 15));
+            muligeUdfald.Add("K", new Udfald("9.161", "55", "uy", 18, 16, 11));
+            muligeUdfald.Add("L", new Udfald("12.699", "79", "sh", 25, 23, 15));
+            muligeUdfald.Add("M", new Udfald("8.886", "55", "sh", 18, 16, 10));
+            muligeUdfald.Add("N", new Udfald("6.350", "40", "sh", 13, 11, 7));
+            muligeUdfald.Add("O", new Udfald("2.735", "17", "sh", 5, 4, 3));
 
        
             //Tjekker i hashtable hvilket udfald det er pga af resultatet fra queryString, og henter de relevante data:
@@ -141,7 +144,7 @@ namespace Ydelsesberegner.Pages
                 case "kh": //Kontanthjælp
                     Text1 = Text1Del1 + ydelseMnd + KHText1Del2 + ydelseTime + Text1Del3 + KHText1Del4;
                     Text2 = KHText2;
-                    ChartTitle = "Kontanthjælp " + ydelseMnd + " kr./mnd";
+                    ChartTitle = "Kontanthjælp: " + ydelseMnd + " kr./md";
                     ChartText = "Diagrammet viser at du blot skal arbejde " +
                                     timerAvis.ToString() +
                                     " timer som avisbud, " +
@@ -156,7 +159,7 @@ namespace Ydelsesberegner.Pages
                 case "uy": //Uddannelsesydelse
                     Text1 = Text1Del1 + ydelseMnd + UYText1Del2 + ydelseTime + Text1Del3 + UYText1Del4;
                     Text2 = UYText2;
-                    ChartTitle = "Uddannelseshjælp " + ydelseMnd + " kr./mnd";
+                    ChartTitle = "Uddannelseshjælp: " + ydelseMnd + " kr./md";
                     ChartText = "Diagrammet viser at du blot skal arbejde " +
                             timerAvis.ToString() +
                             " timer som avisbud, " +
@@ -171,7 +174,7 @@ namespace Ydelsesberegner.Pages
                 case "sh": //selvforsørgelses- og hjemrejseydelse (S&H-ydelse)
                     Text1 = Text1Del1 + ydelseMnd + SHText1Del2 + ydelseTime + Text1Del3 + SHText1Del4;
                     Text2 = SHText2;
-                    ChartTitle = "S&H-ydelse " + ydelseMnd + " kr./mnd";
+                    ChartTitle = "S&H-ydelse: " + ydelseMnd + " kr./md";
                     ChartText = "Diagrammet viser at du blot skal arbejde " +
                             timerAvis.ToString() +
                             " timer som avisbud, " +
